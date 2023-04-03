2979270946a9f10df4f498a135967c6c212a5d5a
saneen <saneen.kp@entri.me>
2023-04-03 21:23:46 +0530

    Type switcher done

--- app/src/main/java/com/example/flickhunt/MainActivity.kt
+++ app/src/main/java/com/example/flickhunt/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,6 +15,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -35,6 +38,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val homeViewModel: HomeViewModel by viewModels()
+    val movieTypeList: List<String> = listOf("", "movie", "series", "episode")
+    private val movieTypeTagList: List<String> = listOf("Home", "Movie", "Series", "Episode")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,24 +50,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    MovieList()
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        MovieTypeSwitcher()
+                        MovieList()
+                    }
                 }
             }
         }
     }
 
     @Composable
-    fun Greeting(name: String) {
-        Text(text = "Hello $name!")
+    fun MovieTypeSwitcher() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(vertical = 20.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+            ) {
+                items(movieTypeTagList.size) { position ->
+                    MovieTypePebble(movieType = movieTypeTagList[position])
+                }
+            }
+        }
     }
 
-    @Preview(showBackground = true)
     @Composable
-    fun DefaultPreview() {
-        FlickHuntTheme {
-            Greeting("Android")
-            MovieList()
+    fun MovieTypePebble(movieType: String) {
+        Card(
+            modifier = Modifier
+                .background(Color(0xFFFFFFFF))
+                .wrapContentHeight()
+                .wrapContentWidth()
+                .padding(4.dp),
+            shape = RoundedCornerShape(13.dp),
+            elevation = 4.dp,
+        ) {
+            Text(
+                modifier = Modifier.padding(12.dp),
+                text = movieType,
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
+            )
         }
     }
 
@@ -93,7 +129,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun SetMovieItem(movie : Movie?){
+    private fun SetMovieItem(movie: Movie?) {
         movie?.let { _movie ->
             MovieView(
                 posterUrl = _movie.Poster,
